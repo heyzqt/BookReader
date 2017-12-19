631f543ca54941c8428f7ab58edfdcfd9dcc83f1
zhangqing <zhangqing02@skyworth.com>
2017-12-19 20:35:57 +0800

    add queryAllBook()

--- app/src/main/java/com/heyzqt/bookreader/MainActivity.java
+++ app/src/main/java/com/heyzqt/bookreader/MainActivity.java
@@ -1,13 +1,50 @@
 package com.heyzqt.bookreader;
 
-import android.support.v7.app.AppCompatActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.heyzqt.booklibrary.BookConstract;
 
 public class MainActivity extends AppCompatActivity {
+
+	private static final String TAG = "MainActivity";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		queryAllBook();
+	}
+
+	private void queryAllBook() {
+		Cursor cursor = getContentResolver().query(BookConstract.Book.CONTENT_URI,
+				null,
+				null,
+				null,
+				null);
+		if (cursor != null) {
+			showAllData(cursor);
+			cursor.close();
+		}
+	}
+
+	private void showAllData(Cursor cursor) {
+		try {
+			if (cursor.getCount() > 0) {
+				while (cursor.moveToNext()) {
+					StringBuffer sb = new StringBuffer();
+					for (int i = 0; i < cursor.getColumnCount(); i++) {
+						sb.append(cursor.getString(i) + ",");
+					}
+					Log.i(TAG, "" + sb);
+				}
+			}
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+			Log.i(TAG, "queryBookById: NullPointerException");
+		}
 	}
 }
